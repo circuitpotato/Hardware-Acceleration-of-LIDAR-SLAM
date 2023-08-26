3fc8e44c76ad29ea68a05bf0f14398280489eaba
circuitpotato <135819236+circuitpotato@users.noreply.github.com>
2023-08-26 18:10:11 +0800

    Update main.c

--- Lynthia/main.c
+++ Lynthia/main.c
@@ -12,34 +12,34 @@
 #define column 1079
 #include <math.h>
 
-float test_input_memory[row][column];
+double test_input_memory[row][column];
 
 typedef struct {
-    float angle_min;
-    float angle_max;
-    float angle_increment;
+    double angle_min;
+    double angle_max;
+    double angle_increment;
     int npoints;
-    float range_min;
-    float range_max;
-    float scan_time;
-    float time_increment;
-    float angles[column]; // Array of angles (dynamic allocation)
+    double range_min;
+    double range_max;
+    double scan_time;
+    double time_increment;
+    double angles[column]; // Array of angles (dynamic allocation)
 } LidarParameters;
 
 LidarParameters SetLidarParameters() {
     LidarParameters lidar;
 
-    lidar.angle_min = (float)-2.351831;
-    lidar.angle_max = (float)2.351831;
-    lidar.angle_increment = (float)0.004363;
+    lidar.angle_min = (double)-2.351831;
+    lidar.angle_max = (double)2.351831;
+    lidar.angle_increment = (double)0.004363;
     lidar.npoints = column;
-    lidar.range_min = (float)0.023;
+    lidar.range_min = (double)0.023;
     lidar.range_max = 60;
-    lidar.scan_time = (float)0.025;
-    lidar.time_increment = (float)1.736112e-05;
+    lidar.scan_time = (double)0.025;
+    lidar.time_increment = (double)1.736112e-05;
 
 
-    float angle = lidar.angle_min;
+    double angle = lidar.angle_min;
     for (int i = 0; i < lidar.npoints; i++) {
         lidar.angles[i] = angle;
         angle += lidar.angle_increment;
@@ -64,18 +64,18 @@ void readDatasetFromFile(const char* filename){
     fp = fopen(filename, "r");
     openFileValidity(fp);     // check if file can be open
 
-    // Read float values from file
+    // Read double values from file
     for (int i = 0; i < row; i++){
         float value;
         if (fscanf(fp, "%f,", &value) != 1) {
-            printf("Failed to read float value %d from the file.\n", i + 1);
+            printf("Failed to read double value %d from the file.\n", i + 1);
             fclose(fp);
             exit(1);
         }
         test_input_memory[i][0] = value;
         for (int k = 1; k < column; k++){
             if (fscanf(fp, "%f,", &value) != 1) {
-                printf("Failed to read float value %d from the file.\n", k + 1);
+                printf("Failed to read double value %d from the file.\n", k + 1);
                 fclose(fp);
                 exit(1);
             }
@@ -87,15 +87,15 @@ void readDatasetFromFile(const char* filename){
 }
 
 typedef struct {
-    float x;
-    float y;
+    double x;
+    double y;
 } CartesianPoint;
 
-CartesianPoint polarToCartesian(float r, float theta) {
+CartesianPoint polarToCartesian(double r, double theta) {
     CartesianPoint cartesian;
 
-    cartesian.x = r * cosf(theta);
-    cartesian.y = r * sinf(theta);
+    cartesian.x = r * cos(theta);
+    cartesian.y = r * sin(theta);
 
     return cartesian;
 }
@@ -108,8 +108,8 @@ typedef struct {
 } ScanData;
 
 // Read a clean scan range
-ScanData readAScan(LidarParameters lidar, float scan[row][column], int idx, int usableRange){
-    float maxRange = (lidar.range_max < (float)usableRange) ? lidar.range_max : (float)usableRange;
+ScanData readAScan(LidarParameters lidar, double scan[row][column], int idx, int usableRange){
+    double maxRange = (lidar.range_max < (double)usableRange) ? lidar.range_max : (double)usableRange;
 
     double xs[row];
     double ys[row];
@@ -139,21 +139,21 @@ ScanData readAScan(LidarParameters lidar, float scan[row][column], int idx, int 
 }
 
 // Transform scan range & pose into world points in world frame
-float * Transform (ScanData scan, const float pose[3], float tscan[][2]) {
-    float tx = pose[0];
-    float ty = pose[1];
-    float theta = pose[2];
+double * Transform (ScanData scan, const double pose[3], double tscan[][2]) {
+    double tx = pose[0];
+    double ty = pose[1];
+    double theta = pose[2];
 
-    float ct = cosf(theta);
-    float st = sinf(theta);
-    float R[2][2] = {{ct, -st}, {st,ct}};
+    double ct = cos(theta);
+    double st = sin(theta);
+    double R[2][2] = {{ct, -st}, {st,ct}};
 
     for (int i = 0; i < scan.size; i++){
 
         // multiply scan(x,y) by transformed R
         // scan is (N,2) matrix and R is (2,2) matrix
-        float transformed_x = (float)(R[0][0] * scan.x[i] + R[1][0] * scan.y[i]);
-        float transformed_y = (float)(R[0][1] * scan.x[i] + R[1][1] * scan.y[i]);
+        double transformed_x = (double)(R[0][0] * scan.x[i] + R[1][0] * scan.y[i]);
+        double transformed_y = (double)(R[0][1] * scan.x[i] + R[1][1] * scan.y[i]);
 
 
 
@@ -169,7 +169,7 @@ float * Transform (ScanData scan, const float pose[3], float tscan[][2]) {
 }
 
 typedef struct {
-    float * pose;
+    double * pose;
     int iBegin;
     int iEnd;
     int loopClosed;
@@ -177,40 +177,59 @@ typedef struct {
 } my_keyscans;
 
 typedef struct {
-    float (*points)[2];
+    int points_size;
+    double (*points)[2];
+
     my_keyscans * keyscans;
 } my_map;
 
 // Initialise map
-void Initialise (my_map *map, float pose[3], ScanData scan){
-    float tscan[scan.size][2];
-
+my_map Initialise (const double pose[3], const ScanData scan) {
+    double tscan[scan.size][2];
     Transform(scan, pose, tscan); // Populate tscan
 
-    map->points = tscan;
-    map->keyscans[0].pose = pose;
-    map->keyscans[0].iBegin = 1;
-    map->keyscans[0].iEnd = scan.size;
-    map->keyscans[0].loopClosed = 1;
-    map->keyscans[0].loopTried = 0;
+    my_map map;
+
+    // Allocate memory for points array and copy values from tscan
+    map.points = malloc(scan.size * sizeof(double[2]));
+    for (int i = 0; i < scan.size; i++) {
+        map.points[i][0] = tscan[i][0];
+        map.points[i][1] = tscan[i][1];
+    }
+
+    map.points_size = scan.size;
+
+    map.keyscans[0].pose = (double *)pose;
+    map.keyscans[0].iBegin = 1;
+    map.keyscans[0].loopClosed = 1;
+    map.keyscans[0].loopTried = 0;
+
+    return map;
 }
 
+
 typedef struct {
-    float* x;
-    float* y;
+    double* x;
+    double* y;
     int size;
 } myLocalMap;
 
-myLocalMap ExtractLocalMap(const my_map map, const float pose[3], const ScanData scan, const float borderSize) {
-    float tscan[scan.size][2];
+myLocalMap ExtractLocalMap(double map_points[][2], int points_size,const double pose[3], ScanData scan, const double borderSize) {
+    double tscan[scan.size][2];
+
+//    for (int p = 0; p < points_size; p++){
+//        printf("%f %f\n", map_points[p][0], map_points[p][1]);
+//    }
 
     // Allocate memory for points array
-    float *xs = (float *) malloc(map.keyscans->iEnd * sizeof(float));
-    float *ys = (float *) malloc(map.keyscans->iEnd * sizeof(float));
+    double *xs = (double *) malloc(points_size * sizeof(double));
+    double *ys = (double *) malloc(points_size* sizeof(double));
 
-//    float xs[map.keyscans->iEnd];
-//    float ys[map.keyscans->iEnd];
+//    double xs[map.keyscans->iEnd];
+//    double ys[map.keyscans->iEnd];
     Transform(scan, pose, tscan); // Populate tscan
+
+
 
 //    //print values
 //    printf("scan size %d\n", scan.size);
@@ -219,10 +238,10 @@ myLocalMap ExtractLocalMap(const my_map map, const float pose[3], const ScanData
 //
 //    }
 
-    float minX = tscan[0][0];
-    float minY = tscan[0][1];
-    float maxX = tscan[0][0];
-    float maxY = tscan[0][1];
+    double minX = tscan[0][0];
+    double minY = tscan[0][1];
+    double maxX = tscan[0][0];
+    double maxY = tscan[0][1];
 
     for (int i = 1; i < scan.size; i++) {
         if (tscan[i][0] < minX) {
@@ -254,12 +273,14 @@ myLocalMap ExtractLocalMap(const my_map map, const float pose[3], const ScanData
 
     // Extract x and y-axis of localMap
     int valid_points = 0;
-    for (int i = 0; i < map.keyscans->iEnd; i++) {
-        if ((map.points[i][0] > minX) && (map.points[i][0] < maxX) && (map.points[i][1] > minY) &&
-            (map.points[i][1] < maxY)) {
+    for (int i = 0; i < points_size; i++) {
+        //printf("%f %f\n", map.points[i][0], map.points[i][1]);
+        if ((map_points[i][0] > minX) && (map_points[i][0] < maxX) && (map_points[i][1] > minY) &&
+            (map_points[i][1] < maxY)) {
 
-            xs[i] = map.points[i][0];
-            ys[i] = map.points[i][1];
+            xs[valid_points] = map_points[i][0];
+            ys[valid_points] = map_points[i][1];
+            //printf("%f %f\n", map_points[i][0], map_points[i][1]);
             valid_points++;
             //printf("%d\n",valid_points);
         }
@@ -273,18 +294,23 @@ myLocalMap ExtractLocalMap(const my_map map, const float pose[3], const ScanData
     localMap_data.y = ys;
     localMap_data.size = valid_points;
 
+//    for (int o = 0; o < localMap_data.size; o++){
+//        printf("local data x = %f\n", localMap_data.x[o]);
+//    }
+//
+//    printf("local map size  = %d\n", valid_points);
     return localMap_data;
 }
 
 
-void euclidean_distance_transform(const int input[], float output[], const int width, const int height) {
-    float MAX_DIST = 10;
-    float precomputedDistances[width * height];
+void euclidean_distance_transform(const int input[], double output[], const int width, const int height) {
+    double MAX_DIST = 10;
+    double precomputedDistances[width * height];
 
     // Precompute squared distances
     for (int j = 0; j < height; ++j) {
         for (int i = 0; i < width; ++i) {
-            precomputedDistances[j * width + i] = (float)(i * i) + (float)(j * j);
+            precomputedDistances[j * width + i] = (double)(i * i) + (double)(j * j);
         }
     }
 
@@ -294,19 +320,19 @@ void euclidean_distance_transform(const int input[], float output[], const int w
             if (input[index] != 0) {
                 output[index] = 0;
             } else {
-                float min_dist_squared = MAX_DIST * MAX_DIST;
+                double min_dist_squared = MAX_DIST * MAX_DIST;
                 for (int j = 0; j < height; ++j) {
                     for (int i = 0; i < width; ++i) {
                         int idx = j * width + i;
                         if (input[idx] != 0) {
-                            float dist_squared = precomputedDistances[idx] + precomputedDistances[index];
+                            double dist_squared = precomputedDistances[idx] + precomputedDistances[index];
                             if (dist_squared < min_dist_squared) {
                                 min_dist_squared = dist_squared;
                             }
                         }
                     }
                 }
-                output[index] = sqrtf(min_dist_squared); // Calculate square root only once
+                output[index] = sqrt(min_dist_squared); // Calculate square root only once
             }
         }
     }
@@ -319,15 +345,15 @@ typedef struct {
     int * occGrid;
     int sizeGridrow;
     int sizeGridcolumn;
-    float * metricMap;
-    float pixelSize;
-    float * topLeftCorner;
+    double * metricMap;
+    double pixelSize;
+    double * topLeftCorner;
 } my_grid;
 
 
-my_grid OccuGrid(const myLocalMap localMap, const float pixelSize){
-    float minXY[2] = {localMap.x[0], localMap.y[0]};
-    float maxXY[2] = {localMap.x[0], localMap.y[0]};
+my_grid OccuGrid(const myLocalMap localMap, const double pixelSize){
+    double minXY[2] = {localMap.x[0], localMap.y[0]};
+    double maxXY[2] = {localMap.x[0], localMap.y[0]};
 
     for(size_t a = 0; a < localMap.size; a++){
         if (localMap.x[a] < minXY[0]){
@@ -349,7 +375,7 @@ my_grid OccuGrid(const myLocalMap localMap, const float pixelSize){
     for (int a = 0; a < 2; a++) {
         minXY[a] -= (3 * pixelSize);
         maxXY[a] += (3 * pixelSize);
-        Sgrid[a] = (int)roundf((maxXY[a] - minXY[a]) / pixelSize) + 1;
+        Sgrid[a] = (int)round((maxXY[a] - minXY[a]) / pixelSize) + 1;
     }
 
     size_t gridSize = (int)Sgrid[0] * (int)Sgrid[1];
@@ -357,16 +383,17 @@ my_grid OccuGrid(const myLocalMap localMap, const float pixelSize){
     int temp_grid[gridSize];
     memset(temp_grid, 0, sizeof(int) * gridSize);
 
-    float hits[2];
-    size_t idx[localMap.size];
+    double hits[2];
+    int idx[localMap.size];
     int *grid = (int *)malloc(sizeof(int) * gridSize);
     //int grid[gridSize];
 
-    for (size_t a = 0; a < localMap.size; a++){
-        hits[0] = roundf((localMap.x[a] - minXY[0]) / pixelSize) + 1;
-        hits[1] = roundf((localMap.y[a] - minXY[1]) / pixelSize) + 1;
+    for (int a = 0; a < localMap.size; a++){
+        hits[0] = round((localMap.x[a] - minXY[0]) / pixelSize) + 1;
+        hits[1] = round((localMap.y[a] - minXY[1]) / pixelSize) + 1;
+        //printf("hits = %d %d\n", (int)hits[0], (int)hits[1]);
 
-        idx[a] = (size_t)( ((hits[0] - 1) * (float)Sgrid[1]) + hits[1] ) - 1;
+        idx[a] = (int)( ((hits[0] - 1) * (double)Sgrid[1]) + hits[1] ) - 1;
         //printf("idx %d = %d\n", a, (int) idx[a]);
         temp_grid[(int) idx[a]] = 1;
     }
@@ -380,9 +407,9 @@ my_grid OccuGrid(const myLocalMap localMap, const float pixelSize){
         }
     }
 
-    //float metric_map[gridSize];
-    float *metric_map = (float *)malloc(sizeof(float) * gridSize);
-    //float metric_map[gridSize];
+    //double metric_map[gridSize];
+    double *metric_map = (double *)malloc(sizeof(double) * gridSize);
+    //double metric_map[gridSize];
     euclidean_distance_transform(grid, metric_map, (int) Sgrid[0], (int) Sgrid[1]);
 
     my_grid gridmap;
@@ -390,27 +417,27 @@ my_grid OccuGrid(const myLocalMap localMap, const float pixelSize){
     gridmap.occGrid = (int *)grid;
     gridmap.sizeGridrow = (int) Sgrid[1];
     gridmap.sizeGridcolumn = (int) Sgrid[0];
-    gridmap.metricMap = (float *)metric_map;
+    gridmap.metricMap = (double *)metric_map;
     gridmap.pixelSize = pixelSize;
     gridmap.topLeftCorner = minXY;
     return gridmap;
 }
 
-float angdiff(const float alpha, const float beta) {
+double angdiff(const double alpha, const double beta) {
     double delta = fmod((beta - alpha + M_PI), 2 * M_PI) - M_PI;
-    return (float)delta;
+    return (double)delta;
 }
 
-void DiffPose(const float pose1[3], const float pose2[3], float dp[3]){
-    for (int i = 0; i < 3; i++){
-        dp[i] = pose2[i] - pose1[i];
-    }
-    dp[2] = angdiff(pose1[2], pose2[2]);
-}
+//void DiffPose(const double pose1[3], const double pose2[3], double dp[3]){
+//    for (int i = 0; i < 3; i++){
+//        dp[i] = pose2[i] - pose1[i];
+//    }
+//    dp[2] = angdiff(pose1[2], pose2[2]);
+//}
 
 /***********Main Code************/
 int main() {
-
+    ScanData scan;
     my_map map;
     my_grid gridmap1;
     my_grid gridmap2;
@@ -418,12 +445,12 @@ int main() {
     int miniUpdated = 0;
 
     // Map parameters
-    float borderSize = 1;
-    float pixelSize = 0.2f;
+    double borderSize = 1;
+    double pixelSize = 0.2f;
 
     // Initialize map values
     map.points = NULL; // Initialize to NULL for safety
-    map.keyscans = malloc(sizeof(my_keyscans)); // Allocate memory for a single my_keyscans element
+    //map.keyscans = malloc(sizeof(my_keyscans)); // Allocate memory for a single my_keyscans element
 
     // Call the function to set Lidar parameters
     LidarParameters lidar = SetLidarParameters();
@@ -435,29 +462,41 @@ int main() {
     /*******Timing Starts here***************/
 
     // Initialise scan
-    float pose[3] = {0,0,0};
-    ScanData scan_0 = readAScan(lidar, test_input_memory, 0, 24);     // read a clean scan range
-    Initialise(&map, pose, scan_0);
-    miniUpdated = 1;
+    double pose[3] = {0.0,0.0,0.0};
+    scan = readAScan(lidar, test_input_memory, 0, 24);     // read a clean scan range
 
+    map = Initialise(pose, scan);
+
+//    for (int p = 0; p < map.points_size; p++){
+//        printf("%f %f\n", map.points[p][0], map.points[p][1]);
+//    }
+
+//    printf("%d \n", map.points_size);
+//    printf("pose = %f %f %f\n", map.keyscans[0].pose[0], map.keyscans[0].pose[1], map.keyscans[0].pose[2]);
+//    printf("iBegin = %d\n", map.keyscans[0].iBegin);
+//    printf("loopClosed = %d\n", map.keyscans[0].loopClosed);
+//    printf("loopTried = %d\n", map.keyscans[0].loopTried);
+
+    miniUpdated = 1;
 
     // initiate first scan
     for (int i = 1; i < row; i++) {
-        ScanData scan = readAScan(lidar, test_input_memory, i, 24);     // read a clean scan range
-        printf("%d\n", i);
+        scan = readAScan(lidar, test_input_memory, i, 24);     // read a clean scan range
+        //printf("%d\n", i);
 
         /*==================================Matching current scan to local map =======================================*/
-        if (miniUpdated == 1){
+        if (miniUpdated == 1 && i == 1){
             //ExtractLocalMap(my_map map, double pose[3], ScanData scan, double borderSize, double localMap[][2])
-            myLocalMap localMap = ExtractLocalMap(map, pose, scan, borderSize);
+            myLocalMap localMap = ExtractLocalMap(map.points, map.points_size, pose, scan, borderSize);
 //                printf("size of localMap = %d\n", localMap.size);
 //                printf("pose = %f %f %f\n", pose[0], pose[1], pose[2]);   // pose
+//
 //                for (int a = 0; a < localMap.size; a++) {
 //                    printf("%f %f\n", localMap.x[a], localMap.y[a]);    // localMap values
 //                }
 
-            gridmap1 = OccuGrid(localMap,pixelSize);
-            gridmap2 = OccuGrid(localMap,pixelSize/2);
+            gridmap1 = OccuGrid(localMap,pixelSize/2);
+            //gridmap2 = OccuGrid(localMap,pixelSize/2);
 
             free(localMap.x);
             free(localMap.y);
@@ -466,15 +505,15 @@ int main() {
 
 //                printf("gridmap2 pixelSize = %f\n", gridmap2.pixelSize);
 
-//                for (int a = 0; a < gridmap1.sizeGridrow; a++){
-//                    for (int b = 0; b < gridmap1.sizeGridcolumn; b++){
-//                        if ((int)gridmap1.metricMap[a * gridmap1.sizeGridcolumn + b] == 0){
-//                            printf("grid[%d][%d]\n", a, b);
-//                            //printf("%d\n", a);
-//                            //printf("%d\n", b);
-//                        }
-//                    }
-//                }
+                for (int a = 0; a < gridmap1.sizeGridrow; a++){
+                    for (int b = 0; b < gridmap1.sizeGridcolumn; b++){
+                        if ((int)gridmap1.metricMap[a * gridmap1.sizeGridcolumn + b] == 0){
+                            printf("grid[%d][%d]\n", a, b);
+                            //printf("%d\n", a);
+                            //printf("%d\n", b);
+                        }
+                    }
+                }
 
 //
 //
@@ -493,13 +532,11 @@ int main() {
         }
         free(gridmap1.metricMap);
         free(gridmap1.occGrid);
-        free(gridmap2.metricMap);
-        free(gridmap2.occGrid);
+//        free(gridmap2.metricMap);
+//        free(gridmap2.occGrid);
     }
 
-
-    free(map.keyscans);
-
+    free(map.points);
     /*******Timing Ends here*****************/
     return 0;
 }
