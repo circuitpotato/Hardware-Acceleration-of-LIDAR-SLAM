c93fe95e5a09c299966d9a956c4322825c6d2426
IBNU1 <zubingr8@gmail.com>
2024-04-18 14:40:01 +0800

    Update Accelereated_Euclidean_Distance_Transform.c

--- Submodule_2/Accelereated_Euclidean_Distance_Transform.c
+++ Submodule_2/Accelereated_Euclidean_Distance_Transform.c
@@ -1,11 +1,45 @@
-void euclidean_distance_transform(int input_map[400][400], float output_distance_map[400][400], int width, int height) {
+void euclidean_distance_transform(int input_map[200][200], float output_distance_map[200][200], int width, int height) {
     // Compute the Euclidean distance transform using a Voronoi algorithm
-
+    float MAX_DIST = 10;
     // Initialize distance grid
     float distance[width][height];
     for (int i = 0; i < width; i++) {
         for (int j = 0; j < height; j++) {
-            distance[i][j] = INFINITY;
+            distance[i][j] = MAX_DIST;
+        }
+    }
+
+    // Compute Voronoi diagram and update distance grid
+    for (int x = 0; x < width; x++) {
+        for (int y = 0; y < height; y++) {
+            if (input_map[x][y] == 1) { // Object cell
+                for (int i = 0; i < width; i++) {
+                    for (int j = 0; j < height; j++) {
+                        double dist = sqrt((x - i) * (x - i) + (y - j) * (y - j));
+                        if (dist < distance[i][j]) {
+                            distance[i][j] = (float)dist;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Copy the distance values to the output map
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            output_distance_map[i][j] = distance[i][j];
+        }
+    }
+}
+void euclidean_distance_transform2(int input_map[400][400], float output_distance_map[400][400], int width, int height) {
+    // Compute the Euclidean distance transform using a Voronoi algorithm
+    float MAX_DIST = 10;
+    // Initialize distance grid
+    float distance[width][height];
+    for (int i = 0; i < width; i++) {
+        for (int j = 0; j < height; j++) {
+            distance[i][j] = MAX_DIST;
         }
     }
 
